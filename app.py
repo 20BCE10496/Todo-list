4c7c6c2c37cedc79f4936b0382432151af2f0101
20BCE10496 <amishagoyal2805@gmail.com>
2023-01-23 00:47:58 +0530

    update

--- app.py
+++ app.py
@@ -39,10 +39,19 @@ def products():
     print(allTodo)
     return 'this is product!'
 
-@app.route('/update/<int:sno>')
+@app.route('/update/<int:sno>',methods=['GET','POST'])
 def update(sno):
-    todo=Todo.query.filter_by(sno=sno).first()
-    return render_template('update.html', todo=todo)
+     if request.method=="POST":
+        title=request.form["title"]
+        desc=request.form["desc"]
+        todo=Todo.query.filter_by(sno=sno).first()
+        todo.title=title
+        todo.desc=desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+        todo=Todo.query.filter_by(sno=sno).first()
+        return render_template('update.html', todo=todo)
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
